6ac912160472f43effb2af10daf8b8405733951d
Andreas Florath <andreas@florath.net>
2019-04-19 17:34:56 +0200

    Added basic command line parsing
    Signed-off-by: Andreas Florath <andreas@florath.net>

---
+++ cxx/Config.hh
@@ -0,0 +1,13 @@
+#ifndef MULTI_TSP_CONFIG_HH
+#define MULTI_TSP_CONFIG_HH
+
+namespace MultiTSP {
+
+  class Config {
+  public:
+    Config(int argc, char *argv[]);
+  };
+ 
+}
+
+#endif
---
+++ cxx/main.cc
@@ -0,0 +1,8 @@
+#include "Config.hh"
+
+int main(int argc, char *argv[]) {
+
+  MultiTSP::Config const config(argc, argv);
+
+  return 0;
+}
